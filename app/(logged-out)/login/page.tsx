87772afb90525d65ea335181288d9836996e3f43
freddy24-7 <freddy.austli@aol.com>
2024-05-24 10:07:48 +0200

    adding login

--- app/(logged-out)/login/page.tsx
+++ app/(logged-out)/login/page.tsx
@@ -1,34 +1,104 @@
 'use client'
 
-import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { PersonStandingIcon } from "lucide-react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+const formSchema = z.object({
+    email: z.string().email(),
+    password: z.string(),
+});
 
 const LoginPage = () => {
 
+    const router = useRouter();
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+    });
+
+    const handleSubmit = (data: z.infer<typeof formSchema>) => {
+        console.log("login validation passed");
+        router.push("/dashboard");
+    };
+
     return (
         <>
-            <PersonStandingIcon size={50}/>
+            <PersonStandingIcon size={50} />
             <Card className="w-full max-w-sm">
                 <CardHeader>
-                    <CardTitle>
-                        Log in
-                    </CardTitle>
-                    <CardDescription>
-                        Login to your SupportMe account
-                    </CardDescription>
+                    <CardTitle>Login</CardTitle>
+                    <CardDescription>Login to your SupportMe account</CardDescription>
                 </CardHeader>
                 <CardContent>
-                    Login form
+                    <Form {...form}>
+                        <form
+                            className="flex flex-col gap-4"
+                            onSubmit={form.handleSubmit(handleSubmit)}
+                        >
+                            <FormField
+                                control={form.control}
+                                name="email"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Email</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="john@doe.com" {...field} />
+                                        </FormControl>
+                                        <FormDescription>
+                                            This is the email address you signed up to SupportMe with
+                                        </FormDescription>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="password"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Password</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="john@doe.com" {...field} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <Button type="submit">Login</Button>
+                        </form>
+                    </Form>
                 </CardContent>
                 <CardFooter className="justify-between">
                     {/* eslint-disable-next-line react/no-unescaped-entities */}
                     <small>Don't have an account?</small>
                     <Button asChild variant="outline" size="sm">
-                        <Link href="/sign-up">
-                            Sign up
-                        </Link>
+                        <Link href="/sign-up">Sign up</Link>
                     </Button>
                 </CardFooter>
             </Card>
